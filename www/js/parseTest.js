b1106b8cef00a4652b904d4a5490e97b945b0489
biran dror <biran.dror@gmail.com>
2015-12-30 22:14:53 +0200

    Testing Parse

---
+++ www/js/parseTest.js
@@ -0,0 +1,6 @@
+Parse.initialize("EoP2P9g5Ic5lc6Mxebgx4FcEA6Ro7AQmsAtKMRUL", "9NY3ogqKjbPQwDz1V5uVTMMaQQar8T5LzCr6HucI");
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});

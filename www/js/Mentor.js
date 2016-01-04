74e4b9f5abbbc2da16b3bf8678c348f1bdbbe72e
biran dror <biran.dror@gmail.com>
2016-01-04 19:50:10 +0200

    added findMentorbyID function

--- www/js/Mentor.js
+++ www/js/Mentor.js
@@ -2,19 +2,40 @@ Parse.initialize("EoP2P9g5Ic5lc6Mxebgx4FcEA6Ro7AQmsAtKMRUL", "9NY3ogqKjbPQwDz1V5
 
 //Saving Objects
 var Mentor = Parse.Object.extend("Mentor");
-
 var mentor = new Mentor();
 
-mentor.save({
-  name: "test name",
-  job: "test job",
-  company: "test company",
-  paragraph: "testing paragraph"
-}, {
-  success: function(mentor) {
-    alert("The object was saved successfully");
+// mentor.save({
+//   name: "test name",
+//   job: "test job",
+//   company: "test company",
+//   paragraph: "testing paragraph"
+// }, {
+//   success: function(mentor) {
+//     alert("The object was saved successfully");
+//   },
+//   error: function(mentor, error) {
+//     alert("The save failed");
+//   }
+// });
+
+findMentorbyJob("Jedi");
+
+/* functions */
+
+function findMentorbyJob(job) {
+  var query = new Parse.Query(Mentor);
+  query.equalTo("job", job);
+  query.find({
+  success: function(results) {
+    alert("Successfully retrieved " + results.length + " metors");
+    // Do something with the returned Parse.Object values
+    for (var i = 0; i < results.length; i++) {
+      var object = results[i];
+      console.log(object.id + ' - ' + object.get('name') + ' - ' + object.get('job') + ' - ' + object.get('company') + ' - ' + object.get('paragraph'));
+    }
   },
-  error: function(mentor, error) {
-    alert("The save failed");
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
   }
 });
+}
--- www/js/parseTest.js
+++ www/js/parseTest.js
@@ -1,10 +1,32 @@
 Parse.initialize("EoP2P9g5Ic5lc6Mxebgx4FcEA6Ro7AQmsAtKMRUL", "9NY3ogqKjbPQwDz1V5uVTMMaQQar8T5LzCr6HucI");
 
 var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
+var newTestObject = new TestObject();
+// testObject.save({test1: "test1.2Data", test2: "test2.2Data", test3: "test3.2Data" }).then(function(object) {
+//    alert("A new testObject was created");
+//  });
+
+var query = new Parse.Query(TestObject);
+query.get("h50KBRTFCf", {
+  success: function(testObject) {
+    alert("The object was retrieved successfully.");
+    testObject.set("test1", "changed1");
+    testObject.set("test2", "changed2");
+    testObject.set("skills", ["ux\\ui","producr managment"])
+    testObject.add("skills", "designer");
+    testObject.save();
+
+  },
+  error: function(object, error) {
+     alert("The object was not retrieved successfully.");
+    // error is a Parse.Error with an error code and message.
+  }
 });
+
+
+
+
+
 
 
 

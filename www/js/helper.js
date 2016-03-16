ce97333e7a6d8bb1230943fc0d3c9b6474c0b8d0
biran dror <biran.dror@gmail.com>
2016-03-16 23:39:00 +0200

    adding video to the mentor page

--- www/js/helper.js
+++ www/js/helper.js
@@ -1,4 +1,4 @@
-var mentorTemplate = '<div class="item"><div class="carousel-caption"><img src="%img%" alt="mentorImg" style="width:200px; height:200px;">'
+var mentorTemplate = '<div class="item"><div class="carousel-caption"><img src="%img%" alt="mentorImg" class="img-circle profileImg">'
 + '<h1>%name%</h1><h4>%company%</h4><h3>%job%</h3><p>%paragraph%</p><button type="button" class="btn btn-primary btn-lg btn-block" id="meetingBTN">Set Up a Meeting</button></div>';
 
 //get a parameter from the url

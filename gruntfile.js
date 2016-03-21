84267c0516c2596a1b31211016ba755db6831f39
daniel818 <avital.daniel@gmail.com>
2016-03-21 13:47:38 +0200

    node modules installed

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,154 @@
+'use strict';
+
 module.exports = function(grunt) {
-  // Do grunt-related things in here
 grunt.loadNpmTasks('grunt-contrib-watch');
 
+
+	// Unified Watch Object
+	var watchFiles = {
+		clientViews: ['www/*.html'],
+		clientJS: ['www/js/*.js'],
+		clientCSS: ['www/css/*.css']
+	};
+
+	// Project Configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			clientViews: {
+				files: watchFiles.clientViews,
+				options: {
+					livereload: true,
+				}
+			},
+			clientJS: {
+				files: watchFiles.clientJS,
+				tasks: ['jshint'],
+				options: {
+					livereload: true
+				}
+			},
+			clientCSS: {
+				files: watchFiles.clientCSS,
+				tasks: ['csslint'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		jshint: {
+			all: {
+				src: watchFiles.clientJS,
+				options: {
+					jshintrc: true
+				}
+			}
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc',
+			},
+			all: {
+				src: watchFiles.clientCSS
+			}
+		},
+		uglify: {
+			production: {
+				options: {
+					mangle: false
+				},
+				files: {
+					'dist/application.min.js': 'dist/application.js'
+				}
+			}
+		},
+		cssmin: {
+			combine: {
+				files: {
+					'dist/application.min.css': '<%= applicationCSSFiles %>'
+				}
+			}
+		},
+		'node-inspector': {
+			custom: {
+				options: {
+					'web-port': 1337,
+					'web-host': 'localhost',
+					'debug-port': 5858,
+					'save-live-edit': true,
+					'no-preload': true,
+					'stack-trace-limit': 50,
+					'hidden': []
+				}
+			}
+		},
+		ngAnnotate: {
+			production: {
+				files: {
+					'dist/application.js': '<%= applicationJavaScriptFiles %>'
+				}
+			}
+		},
+		concurrent: {
+			default: [ 'watch'],
+			debug: ['nodemon', 'watch', 'node-inspector'],
+			options: {
+				logConcurrentOutput: true,
+				limit: 10
+			}
+		},
+		env: {
+			test: {
+				NODE_ENV: 'test'
+			},
+			secure: {
+				NODE_ENV: 'secure'
+			}
+		},
+		mochaTest: {
+			src: watchFiles.mochaTests,
+			options: {
+				reporter: 'spec',
+				require: 'server.js'
+			}
+		},
+		karma: {
+			unit: {
+				configFile: 'karma.conf.js'
+			}
+		}
+	});
+
+	// Load NPM tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Making grunt default to force in order not to break the project.
+	grunt.option('force', true);
+
+	// A Task for loading the configuration object
+	grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
+		var init = require('./config/init')();
+		var config = require('./config/config');
+
+		grunt.config.set('applicationJavaScriptFiles', config.assets.js);
+		grunt.config.set('applicationCSSFiles', config.assets.css);
+	});
+
+	// Default task(s).
+	grunt.registerTask('default', ['lint', 'concurrent:default']);
+
+	// Debug task.
+	grunt.registerTask('debug', ['lint', 'concurrent:debug']);
+
+	// Secure task(s).
+	grunt.registerTask('secure', ['env:secure', 'lint', 'concurrent:default']);
+
+	// Lint task(s).
+	grunt.registerTask('lint', ['jshint', 'csslint']);
+
+	// Build task(s).
+	grunt.registerTask('build', ['lint', 'loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
+
+	// Test task.
+	grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
 };

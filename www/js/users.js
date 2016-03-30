6673cb25d0c4212ea130d40df48e475e2d6b4832
daniel818 <avital.daniel@gmail.com>
2016-03-30 09:51:16 +0300

    changing meeting btn

--- www/js/users.js
+++ www/js/users.js
@@ -1,12 +1,10 @@
 Parse.initialize("EoP2P9g5Ic5lc6Mxebgx4FcEA6Ro7AQmsAtKMRUL", "9NY3ogqKjbPQwDz1V5uVTMMaQQar8T5LzCr6HucI");
 
 
-$(".");
 
-function meetingBTN(e){
-console.log(e);
-    // var mentorID = document.getElementsByClassName("mentorID");
+function meetingBTN(){
 
+    var mentorID = document.getElementsByClassName("mentorID");
     var i = $(".active").index();
     console.log(mentorID[i].textContent);
     alert(mentorID[i].textContent);

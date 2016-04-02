84746e689073e7c71d5fc93938af3b460d9c84e7
daniel818 <avital.daniel@gmail.com>
2016-04-03 01:37:37 +0300

    save mail addresses before sending the message

--- www/js/users.js
+++ www/js/users.js
@@ -1,10 +1,23 @@
 Parse.initialize("EoP2P9g5Ic5lc6Mxebgx4FcEA6Ro7AQmsAtKMRUL", "9NY3ogqKjbPQwDz1V5uVTMMaQQar8T5LzCr6HucI");
 
+var mentorIdString;
+var mentorMail;
+
 
 
 function meetingBTN(){
+
     var mentorID = document.getElementsByClassName("mentorID");
     var i = $(".active").index();
     console.log(mentorID[i].textContent);
     alert(mentorID[i].textContent);
+    mentorIdString = mentorID[i].textContent;
+    retrieveMentorInfo(mentorIdString);
+    window.location = "Users.html" + "?mentorID=" + mentorIdString + "&";
+}
+
+function saveForMail() {
+    var temp = getParameterByName('mentorID');
+    var mentorMail = retrieveMentorInfo(temp);
+
 }

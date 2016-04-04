1bcf5431400f89bb2cae1223ec62b3cf4cb61c65
biran dror <biran.dror@gmail.com>
2016-04-04 17:05:09 +0300

    adding the wow effect to the carousel

--- www/js/helper.js
+++ www/js/helper.js
@@ -1,10 +1,14 @@
-var mentorTemplate = '<div class="item"><div class="carousel-caption"><img src="%img%" alt="mentorImg" class="img-circle profileImg">'
-+ '<h1>%name%</h1><h4>%company%</h4><h3>%job%</h3><p>%paragraph%</p>'
-+ '<button type="button" class="btn btn-primary btn-lg btn-block" class="meetingBTN" onclick="meetingBTN()">Set Up a Meeting</button>'
-    + '<div class="mentorID" style="display: none;">%id%</div></div>';
+//mentor template to append
+ var mentorTemplate =
+'<div class="item"><div class="carousel-cover"><h2>%name%</h2><h3>%job%</h3><img src="%img%" alt="mentorImg" class="profileImg">' +
+'</div><div class="carousel-caption"><div class="row"><div class="col-xs-4 col-sm-4""><div class="company">12</div><div class="label">MEETINGS</div>' +
+'<div class="marker purpel"></div></div><div class="col-xs-8 col-sm-8""><div class="company">%company%</div><div class="label">COMPANY</div>' +
+'<div class="marker"></div></div></div><div class="line"></div><div class="paragraph">%paragraph%</div><div class="mentorID" style="display: none;">%id%</div>' +
+'</div><button type="button" class="meetingBTN" id="meetingBTN" onclick="meetingBTN()"><img src="images/Calendar-50.png" class="calender-icon">' +
+'Set Up a Meeting</button><div class="mentorID" style="display: none;">%id%</div></div></div>';
+
 
 //get a parameter from the url
-
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");

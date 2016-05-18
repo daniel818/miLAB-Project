e113ba671170ece078449a396c0e67fc88d590ce
daniel818 <avital.daniel@gmail.com>
2016-05-18 09:28:24 +0300

    mail test

--- www/mailsrv.php
+++ www/mailsrv.php
@@ -1,2 +1,9 @@
 <?php
-print 2+2;
+$to      = 'avital.daniel@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: info@yodapp.co.il' . "\r\n" .
+    'Reply-To: eranilani@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);

3546b5f42d9e4c85eeec0714f102882c53cdf1ce
daniel818 <avital.daniel@gmail.com>
2016-05-24 13:08:00 +0300

    testing parse

--- www/mailsrv.php
+++ www/mailsrv.php
@@ -4,11 +4,11 @@ require 'vendor/autoload.php';
 use Parse\ParseClient;
 
 ParseClient::initialize('EoP2P9g5Ic5lc6Mxebgx4FcEA6Ro7AQmsAtKMRUL', '', 'zZeJlsPDd1mBxsRojtyX3QJi9RfynZI4m9soO9xM');
-ParseClient::setServerURL('https://api.parse.com/1/');
+ParseClient::setServerURL('https://api.parse.com/parse/');
 
 use Parse\ParseObject;
 
-$restObject = ParseObject::create("RestObject");
+$restObject = ParseObject::create("TestObject");
 $restObject->set("foo", "bar");
 $restObject->save();
 
